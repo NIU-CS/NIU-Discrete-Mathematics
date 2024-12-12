b7bb2898de928a4cad3aec30198c927fb9163a32
Young <young20050727@gmail.com>
2024-12-12 14:50:47 +0800

    add SetOperations.hs

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,15 @@
 module Main (main) where
 
 import Lib
+import SetOperations (cartesianProduct, powerSetSize, a, b)
 
 main :: IO ()
-main = modd
+main = do
+  -- 計算 A x B
+  let axb = cartesianProduct a b
+  let sizeOfAxB = length axb
+  let sizeOfPowerSet = powerSetSize sizeOfAxB
+
+  -- 輸出結果
+  putStrLn $ "Size of A x B: " ++ show sizeOfAxB
+  putStrLn $ "Size of P(A x B): " ++ show sizeOfPowerSet
