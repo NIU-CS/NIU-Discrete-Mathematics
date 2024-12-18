885f7191fc0e5dab2306374c1d2b7b310ea907f5
young <young20050727@gmail.com>
2024-12-18 23:42:23 +0800

    Counting.hs 1a, 1b

--- src/Counting.hs
+++ src/Counting.hs
@@ -24,22 +24,38 @@ module Counting
     -- Entry point for demonstration
     computeCounting :: IO ()
     computeCounting = do
-        -- (a) In how many ways can the letters in DATAGRAM be arranged?
+        -- 0. (a) In how many ways can the letters in DATAGRAM be arranged?
         let n = 8
         let r = 5
         putStrLn $ "In how many ways can the letters in DATAGRAM be arranged?"
         putStrLn $ "P(" ++ show n ++ ", " ++ show r ++ ") = " ++ show (p n r)
 
-        -- (b) For the arrangements of part(a), how many have all three A's together?
+        -- 0. (b) For the arrangements of part(a), how many have all three A's together?
         let n = 6
         putStrLn $ "For the arrangements of part(a), how many have all three A's together?"
         putStrLn $ "6! = " ++ show (frac n)
 
+        --   A B   |   G H   |   F G
+        -- H     C | F     A | E     H
+        -- G     D | E     B | D     A
+        --   F E   |   D C   |   C B
+        -- (a)     | (b)     | (c)
 
+        -- 1. (a) In how many ways can eight people, denoted A, B, ..., H, be seated about the square table, where situations (a) and (b) are considered the same, but are distinct from (c).
+        putStrLn $ "In how many ways can eight people, denoted A, B, ..., H, be seated about the square table, where situations (a) and (b) are considered the same, but are distinct from (c)."
+        -- 2 * 7! = 10080
+        putStrLn $ "P(" ++ show n ++ ", " ++ show r ++ ") = " ++ show (2 * frac 7)
 
-        let n = 5
-        let r = 3
-        let result1 = p n r
-        let result2 = c n r
-        putStrLn $ "P(" ++ show n ++ ", " ++ show r ++ ") = " ++ show result1
-        putStrLn $ "C(" ++ show n ++ ", " ++ show r ++ ") = " ++ show result2
+        -- 1. (b) If two of the eight people, say A and B, do not get along well, how many different seatings are possible with A and B not sitting next to each other?
+        let n = 7
+        let r = 7
+        putStrLn $ "If two of the eight people, say A and B, do not get along well, how many different seatings are possible with A and B not sitting next to each other?"
+        -- 10080 - 4 * 6! = 7200
+        putStrLn $ show 10080 ++ " - 4 * 6! = " ++ show (10080 - 4 * frac 6)
+
+        -- let n = 5
+        -- let r = 3
+        -- let result1 = p n r
+        -- let result2 = c n r
+        -- putStrLn $ "P(" ++ show n ++ ", " ++ show r ++ ") = " ++ show result1
+        -- putStrLn $ "C(" ++ show n ++ ", " ++ show r ++ ") = " ++ show result2
