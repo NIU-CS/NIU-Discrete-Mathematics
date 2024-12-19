96c41d35a30a8b8938fb83c2c9ad6a0908fa446e
Young <young20050727@gmail.com>
2024-12-19 14:32:37 +0800

    update Main.hs(add computeLogic)

--- app/Main.hs
+++ app/Main.hs
@@ -7,17 +7,18 @@ import Logic (computeLogic)
 
 main :: IO ()
 main = do
-  -- 計算 A x B
-  let axb = cartesianProduct a b
-  let sizeOfAxB = length axb
-  let sizeOfPowerSet = powerSetSize sizeOfAxB
+    -- 計算 A x B
+    let axb = cartesianProduct a b
+    let sizeOfAxB = length axb
+    let sizeOfPowerSet = powerSetSize sizeOfAxB
 
-  -- 輸出結果
-  putStrLn $ "Size of A x B: " ++ show sizeOfAxB
-  putStrLn $ "Size of P(A x B): " ++ show sizeOfPowerSet
+    -- 輸出結果
+    putStrLn $ "Size of A x B: " ++ show sizeOfAxB
+    putStrLn $ "Size of P(A x B): " ++ show sizeOfPowerSet
 
-  -- 計算 3^21 mod 53
-  computeModularExponentiation
 
-  -- 計算 P(5, 3) 和 C(5, 3)
-  computeCounting
+    computeModularExponentiation
+
+    computeCounting
+
+    computeLogic
