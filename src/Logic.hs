571caa7a140ce87486481f7a345d4bca176faade
Young <young20050727@gmail.com>
2024-12-19 14:13:17 +0800

    Logic.hs 1

--- src/Logic.hs
+++ src/Logic.hs
@@ -1,3 +1,11 @@
 module Logic (
 
 ) where
+
+-- 1. Determine whether each of the following sentences is a statement. If a sentence is a statement, determine whether it is true or false. If a sentence is a statement.
+-- a) In 2023 George W. Bush was the president of the United States.
+-- b) x + 3 is a positive interger.
+-- c) Fifteen is an even number.
+-- d) If Jennifer is late for the party, then her cousin Zachary will be quite angry.
+-- e) What time is it?
+-- f) As of June 30, 2003, Christine Marie Evert had won the French Open a record seven times.
