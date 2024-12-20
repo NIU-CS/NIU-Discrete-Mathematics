d331c479ad921eb5d853e4d7f2f4c584264d7268
young <young20050727@gmail.com>
2024-12-20 16:27:51 +0800

    Number.hs 2, 3

--- src/Number.hs
+++ src/Number.hs
@@ -15,4 +15,25 @@ computeNumber = do
     putStrLn "1. For all a, b, c ∈ Z"
     putStrLn "a | b => a | bx for all x ∈ Z"
 
-    -- 2.
+    -- 2. Prove Theorem 4.5 (a).
+    -- Let a_1, a_2, b_1, b_2, and n be integers with n > 1.
+    -- If a_1 ≡ a_2 (mod n) and b_1 ≡ b_2 (mod n), then
+    -- (a) a_1 + b_1 ≡ a_2 + b_2 (mod n), and
+    --
+    -- Proof: a_1 ≡ a_2 (mod n) and b_1 ≡ b_2 (mod n)
+    -- Mean n | (a_1 - a_2) and n | (b_1 - b_2).
+    -- By Theorem 4.1 (e) we have
+    --     n | (a_1 - a_2) + (b_1 - b_2)
+    --  => n | (a_1 + b_1) - (a_2 + b_2)
+    -- Thus, a_1 + b_1 ≡ a_2 + b_2 (mod n)
+
+    putStrLn "2. Prove Theorem 4.5 (a)."
+    putStrLn "Let a_1, a_2, b_1, b_2, and n be integers with n > 1."
+    putStrLn "If a_1 ≡ a_2 (mod n) and b_1 ≡ b_2 (mod n), then"
+    putStrLn "(a) a_1 + b_1 ≡ a_2 + b_2 (mod n), and"
+
+    -- 3. Compute 3^21 mod 53
+
+    putStrLn "3. Compute 3^21 mod 53"
+    let result = 3 ^ 21 `mod` 53
+    putStrLn $ "3^21 mod 53 = " ++ show result
