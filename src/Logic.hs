392f05144d2a6f7cd5ce59ce1b241fa017aeda08
young <young20050727@gmail.com>
2024-12-23 04:41:25 +0800

    use functions to wrap problems in Logic.hs

--- src/Logic.hs
+++ src/Logic.hs
@@ -15,21 +15,9 @@ data Prop a where
     Iff          :: Prop Bool -> Prop Bool -> Prop Bool
     ExclusiveNor :: Prop Bool -> Prop Bool -> Prop Bool
 
-computeLogic :: IO ()
-computeLogic = do
-    putStrLn "=============================="
-    putStrLn "Logic"
-    putStrLn "=============================="
-    putStrLn ""
-
-    -- 1. Determine whether each of the following sentences is a statement. If a sentence is a statement, determine whether it is true or false. If a sentence is a statement.
-    -- a) In 2023 George W. Bush was the president of the United States.
-    -- b) x + 3 is a positive interger.
-    -- c) Fifteen is an even number.
-    -- d) If Jennifer is late for the party, then her cousin Zachary will be quite angry.
-    -- e) What time is it?
-    -- f) As of June 30, 2003, Christine Marie Evert had won the French Open a record seven times.
-
+-- 1. Determine whether each of the following sentences is a statement. If a sentence is a statement, determine whether it is true or false. If a sentence is a statement.
+judgeIsStatement :: IO ()
+judgeIsStatement = do
     putStrLn $ "1. Determine whether each of the following sentences is a statement. If a sentence is a statement, determine whether it is true or false. If a sentence is a statement."
     putStrLn $ "a) In 2023 George W. Bush was the president of the United States."
     putStrLn $ "Statement: True"
@@ -44,28 +32,40 @@ computeLogic = do
     putStrLn $ "f) As of June 30, 2003, Christine Marie Evert had won the French Open a record seven times."
     putStrLn $ "Statement: True"
 
-    -- 2. Identify the primitive statements in Exercise 1.
-    -- a) George W. Bush was the president of the United States in 2023.
-    -- b) x + 3 is a positive interger.
-    -- c) Fifteen is an even number.
-    -- d) Jennifer is late for the party.
-    -- e) Zachary will be quite angry.
-    -- f) Christine Marie Evert had won the French Open a record seven times as of June 30, 2003.
+-- 2. Identify the primitive statements in Exercise 1.
+identifyPrimitiveStatements :: IO ()
+identifyPrimitiveStatements = do
+    putStrLn $ "2. Identify the primitive statements in Exercise 1."
+    putStrLn $ "a) George W. Bush was the president of the United States in 2023."
+    putStrLn $ "b) x + 3 is a positive interger."
+    putStrLn $ "c) Fifteen is an even number."
+    putStrLn $ "d) Jennifer is late for the party."
+    putStrLn $ "e) Zachary will be quite angry."
+    putStrLn $ "f) Christine Marie Evert had won the French Open a record seven times as of June 30, 2003."
 
+-- 3. Let p, q, r, s denote the following statements:
+writeStatementSybolic :: IO ()
+writeStatementSybolic = do
+    putStrLn $ "3. Let p, q, r, s denote the following statements:"
+    putStrLn $ "p: I finish writing my computer program before lunch."
+    putStrLn $ "q: I shall play tenis in the afternoon."
+    putStrLn $ "r: The sun is shining."
+    putStrLn $ "s: The humidity is low."
 
-    -- 3. Let p, q, r, s denote the following statements:
-    -- p: I finish writing my computer program before lunch.
-    -- q: I shall play tenis in the afternoon.
-    -- r: The sun is shining.
-    -- s: The humidity is low.
+    putStrLn $ "Write the following in symbolic form."
+    putStrLn $ "a) If the sun is shining, I shall play tennis this afternoon."
+    putStrLn $ "b) Finishing the writing of my computer program before lunch is necessary for my playing tennis this afternoon."
+    putStrLn $ "c) Low humidity and sunshine are sufficient for me to play tennis this afternoon."
 
-    -- Write the following in symbolic form.
-    -- a) If the sun is shining, I shall play tennis this afternoon.
-    -- b) Finishing the writing of my computer program before lunch is necessary for my playing tennis this afternoon.
-    -- c) Low humidity and sunshine are sufficient for me to play tennis this afternoon.
+-- 4.
 
-    -- a) r -> q
-    -- b) q -> p
-    -- c) (s ^ r) -> q
+computeLogic :: IO ()
+computeLogic = do
+    putStrLn "=============================="
+    putStrLn "Logic"
+    putStrLn "=============================="
+    putStrLn ""
 
-    -- 4.
+    judgeIsStatement
+    identifyPrimitiveStatements
+    writeStatementSybolic
