fc51ba9194445d7b1c7befe0c3230c785972f7d6
Young <young20050727@gmail.com>
2024-12-23 16:29:10 +0800

    move comments in ReflectionAndFunctions.hs out of compute function

--- src/ReflectionAndFunction.hs
+++ src/ReflectionAndFunction.hs
@@ -2,11 +2,11 @@ module ReflectionAndFunction (
     computeReflectionAndFunction,
 ) where
 
+-- 1.
+
 computeReflectionAndFunction :: IO ()
 computeReflectionAndFunction = do
     putStrLn "=============================="
     putStrLn "Reflection and Function"
     putStrLn "=============================="
     putStrLn ""
-
-    -- 1.
